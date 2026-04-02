64aa4385987ce0f12e3f8cb22682c85d63cbc323
Umid <umidjorayev091@gmail.com>
2026-04-02 17:31:06 +0500

    brilga topshiriqlar bajarildi

--- products/urls.py
+++ products/urls.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from email.policy import default
+
+from fastapi import APIRouter,Request
 
 router =APIRouter(prefix='/products')
 
@@ -10,3 +12,10 @@ async def products_list():
 @router.get('/test')
 async def products_list():
     return {'test': 'test'}
+
+@router.post('age')
+async def get_age(request: Request ):
+    data = await request.json()
+    return data
+
+
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
+from fastapi import FastAPI,Request
 
-router =APIRouter(prefix='/auth')
+
+router =APIRouter(prefix='/users')
 
 @router.get('/')
 async def sign_up():
@@ -9,3 +11,13 @@ async def sign_up():
 @router.get('/login')
 async def sign_up():
     return {"message": 'login'}
+
+
+@router.post('/data')
+async def data_get(request: Request):
+    data = await request.json()
+    return data
+
+
+
+
